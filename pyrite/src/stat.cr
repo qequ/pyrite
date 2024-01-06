e9ccc1e033dd0e16369d4663bf962e534a7be507
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2024-01-06 16:13:37 -0300

    add uniform and mvuniform
    add tests
    
    Signed-off-by: Alvaro Frias Garay <alvarofriasgaray@gmail.com>

--- pyrite/src/stat.cr
+++ pyrite/src/stat.cr
@@ -59,5 +59,46 @@ module Pyrite
         mu + sigma * z
       end
     end
+
+    class Uniform < Distribution
+      property a : Float64
+      property b : Float64
+
+      def initialize(@a : Float64, @b : Float64)
+        raise ArgumentError.new("a < b is required") unless a < b
+      end
+
+      def pdf(x : Float64) : Float64
+        1.0 / (b - a)
+      end
+
+      def mean : Float64
+        0.5 * (a + b)
+      end
+
+      def variance : Float64
+        (b - a)**2 / 12.0
+      end
+
+      def sample(rng : Random = Random::DEFAULT) : Float64
+        a + (b - a) * rng.rand
+      end
+    end
+
+    class MVUniform
+      property a : Array(Float64)
+      property b : Array(Float64)
+
+      def initialize(@a : Array(Float64), @b : Array(Float64))
+        raise ArgumentError.new("a and b must have the same size") if a.size != b.size
+        a.zip(b).each_with_index do |(ai, bi), i|
+          raise ArgumentError.new("a < b is required for all elements") unless ai < bi
+        end
+      end
+
+      def sample(rng : Random = Random::DEFAULT) : Array(Float64)
+        a.zip(b).map { |ai, bi| ai + (bi - ai) * rng.rand }
+      end
+    end
   end
 end
