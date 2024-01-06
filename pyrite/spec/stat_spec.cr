9c5eca0ab224695948496b404f737b0c615db60f
Alvaro Frias Garay <alvarofriasgaray@gmail.com>
2024-01-06 16:08:04 -0300

    add normal distribution
    add tests
    
    Signed-off-by: Alvaro Frias Garay <alvarofriasgaray@gmail.com>

--- pyrite/spec/stat_spec.cr
+++ pyrite/spec/stat_spec.cr
@@ -21,3 +21,45 @@ describe Pyrite::Stat::Exponential do
     # Additional tests can be added here for other methods like pdf, sample, etc.
   end
 end
+
+describe Pyrite::Stat::Normal do
+  describe "Normal distribution" do
+    # Test for PDF at mean
+    it "calculates the PDF correctly at mean" do
+      mu = 0.0
+      sigma = 1.0
+      normal_dist = Pyrite::Stat::Normal.new(mu, sigma)
+      pdf_value = normal_dist.pdf(mu)
+      expected_pdf = 1 / Math.sqrt(2 * Math::PI)
+      pdf_value.should be_close(expected_pdf, 1e-6)
+    end
+
+    # Test for correct mean
+    it "has the correct mean" do
+      mu = 1.5
+      sigma = 2.0
+      normal_dist = Pyrite::Stat::Normal.new(mu, sigma)
+      normal_dist.mean.should eq(mu)
+    end
+
+    # Test for correct variance
+    it "has the correct variance" do
+      mu = 1.5
+      sigma = 2.0
+      normal_dist = Pyrite::Stat::Normal.new(mu, sigma)
+      normal_dist.variance.should eq(sigma**2)
+    end
+
+    # Test for plausible sample generation
+    it "generates a plausible sample" do
+      mu = 0.0
+      sigma = 1.0
+      normal_dist = Pyrite::Stat::Normal.new(mu, sigma)
+      rng = Random.new
+      sample = normal_dist.sample(rng)
+
+      # Use abs directly on the number object
+      (sample - mu).abs.should be <= 3 * sigma # Within 3 standard deviations
+    end
+  end
+end
--- pyrite/src/stat.cr
+++ pyrite/src/stat.cr
@@ -4,7 +4,7 @@ module Pyrite
     abstract class Distribution
       # Common methods that all distributions will have
       abstract def pdf(x : Float64) : Float64
-      abstract def sample(rng) : Float64
+      abstract def sample(rng : Random = Random::DEFAULT) : Float64
       abstract def mean : Float64
       abstract def variance : Float64
     end
@@ -21,7 +21,7 @@ module Pyrite
         @lambda * Math.exp(-@lambda * x)
       end
 
-      def sample(rng) : Float64
+      def sample(rng : Random = Random::DEFAULT) : Float64
         -Math.log(rng.rand) / @lambda
       end
 
@@ -33,6 +33,31 @@ module Pyrite
         1.0 / (@lambda**2)
       end
     end
-    
+
+    class Normal < Stat::Distribution
+      property mu : Float64
+      property sigma : Float64
+
+      def initialize(@mu : Float64, @sigma : Float64)
+        raise ArgumentError.new("sigma must be positive") if @sigma <= 0
+      end
+
+      def pdf(x : Float64) : Float64
+        1 / (sigma * ::Math.sqrt(2 * ::Math::PI)) * ::Math.exp(-0.5 * ((x - mu) / sigma)**2)
+      end
+
+      def mean : Float64
+        mu
+      end
+
+      def variance : Float64
+        sigma**2
+      end
+
+      def sample(rng : Random = Random::DEFAULT) : Float64
+        z = Pyrite::Math.iphi(rng.rand) # Using the iphi method from the Math module
+        mu + sigma * z
+      end
+    end
   end
 end
